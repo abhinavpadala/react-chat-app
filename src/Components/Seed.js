8f35a265e1f847c51fea312a1e7d845c4b2fb9da
Abhinav Padala <abhinavpadala@live.com>
2021-01-26 08:52:45 +0100

    Refactor: Move mockData to Seed.js

---
+++ src/Components/Seed.js
@@ -0,0 +1,28 @@
+export const mockData = {
+  Nildo: [
+    {
+      id: 0,
+      message: "First message: This is the first message from the other user.",
+      fromMe: false,
+    },
+    {
+      id: 1,
+      message:
+        "Second message: this is the first message from me. Second message: this is the first message from me.",
+      fromMe: true,
+    },
+  ],
+
+  Daniel: [
+    {
+      id: 0,
+      message: "First message: This is the first message from the other user.",
+      fromMe: false,
+    },
+    {
+      id: 1,
+      message: "Second message: this is the first message from me.",
+      fromMe: true,
+    },
+  ],
+};
